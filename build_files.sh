9bd45e37fcc3d1960930d7ca3bf8052fc33b55f0
preciousimo <preciousimoniakemu@gmail.com>
2022-11-22 09:40:18 +0100

    Fixes to build files configuration

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo " BUILD START"
-python3.10.8 -m pip install -r requirements.txt
-python3.10.8 manage.py collectstatic --noinput --clear
+pip install -r requirements.txt
+python manage.py collectstatic --noinput --clear
 echo " BUILD END"
